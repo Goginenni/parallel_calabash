3582f0f36c658edfe4c0c6b9b0b73807abf3afcd
Sathish Gogineni <sathish.gogineni@corp.badoo.com>
2015-06-22 15:09:05 +0100

    Fixed grouping issue

--- lib/parallel_appium.rb
+++ lib/parallel_appium.rb
@@ -27,7 +27,7 @@ module ParallelAppium
       test_results = nil
       report_time_taken do
 
-        groups = FeatureGrouper.feature_groups_by_scenarios(options[:feature_folder], number_of_processes)
+        groups = FeatureGrouper.feature_groups(options, number_of_processes)
 
         threads = groups.size
 
